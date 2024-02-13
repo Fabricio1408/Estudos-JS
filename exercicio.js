45851fd45a6e4295fa3e8c5a0a6ad82c6977decd
Fabricio Da Silva Ribeiro <102611358+Fabricio1408@users.noreply.github.com>
2024-02-13 19:40:35 -0300

    Update exercicio.js

--- exercicio.js
+++ exercicio.js
@@ -62,3 +62,8 @@ fetch(apiUrl)
     console.error('Erro:', error);
   });
 //Estudar requisição Fetch-API
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Erro:', error));
+
